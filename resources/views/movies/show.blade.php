32bbb1f61c8fdbf4b5b18e718e97a8d88ec21da0
ChiaraS97 <chiara.sucato97@gmail.com>
2023-05-23 16:42:27 +0200

    change single card

--- resources/views/movies/show.blade.php
+++ resources/views/movies/show.blade.php
@@ -1,10 +1,20 @@
 @extends('layouts.app')
 
 @section('content')
-<section class="container d-flex flex-column align-items-center">
-    <h1>{{ $movie->title }}</h1>
-    <img src="{{ $movie->image }}" alt="{{ $movie->title }}" class="w-25">
-    <p>{{ $movie->plot }}</p>
+<section class="container">
+    <div class="d-flex flex-column align-items-center mb-5">
+        <h1>{{ $movie->title }}</h1>
+        <img src="{{ $movie->image }}" alt="{{ $movie->title }}" class="w-25">
+    </div>
+    <div class="pb-5">
+        <h3>Trama:</h3>
+        <p>{{ $movie->plot }}</p>
+        <h3>Data di uscita:</h3>
+        <p>{{$movie->date}}</p>
+        <h3>Voto degli utenti:</h3>
+        <p>{{$movie->vote}}</p>
+    </div>
+
 
 </section>
 @endsection
